d56535fc05ea5429859da586abca7e2cdb33cd50
incognito1025 <nkhanamart@gmail.com>
2024-07-26 12:43:38 -0400

    updated previous App.jsx routes to reflect changing  to . Added minor style/css  in App.css

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,40 @@
-import { useState, useEffect } from 'react'
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import './App.css';
 
-//Components
 import NavBar from './Components/NavBar';
-
-//Pages
-import Home from './Pages/Home';
-import Index from './Pages/Index';
+import Edit from "./Pages/Edit";
+import Error from "./Pages/Error";
+import Home from "./Pages/Home";
+import Index from "./Pages/Index";
+import Add from "./Pages/Add";
+import Show from "./Pages/Show";
+import About from "./Pages/About";
+import Login from "./Pages/Login";
 
 function App() {
 
-  const API = import.meta.env.VITE_API_URL;
-  const [ dataArr, setDataArr ] = useState([])
-  
-  useEffect(() => {
-    fetch(`${API}/stats`).then(res => {
-      return res.json();
-    })
-    .then(res => setDataArr(res));
-  },[])
-
   return (
-    <Router>
-      <NavBar/>
-      <Routes>
-        <Route path="/" element={<Home />}/>
-        <Route path="/index" element={<Index dataArr={dataArr}/>}/>
-      </Routes>
-    </Router>
-  )
+    <div>
+      <Router>
+        {/* NavBar is only included on pages other than the landing page */}
+        <main>
+          <Routes>
+            <Route path="/" element={<Login />} />
+            <Route element={<NavBar />}>
+              <Route path="/home" element={<Home />} />
+              <Route path="/about" element={<About />} />
+              <Route path="/players" element={<Index />} />
+              <Route path="/players/add" element={<Add />} />
+              <Route path="/players/:id" element={<Show />} />
+              <Route path="/players/:id/edit" element={<Edit />} />
+            </Route>
+            <Route path="*" element={<Error />} />
+          </Routes>
+        </main>
+      </Router>
+      {/* <h1>Welcome to Fantasy Soccer!</h1> */}
+    </div>
+  );
 }
 
-export default App
+export default App;

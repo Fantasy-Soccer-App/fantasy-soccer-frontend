0ee42e32ce8e38595193d2d0cb05f9fd53818376
incognito1025 <nkhanamart@gmail.com>
2024-07-26 12:24:44 -0400

    created NavBar via src/Components/NavBar.jsx src/CSS/NavBar.css

--- src/Components/NavBar.jsx
+++ src/Components/NavBar.jsx
@@ -1,13 +1,43 @@
 import "../CSS/NavBar.css"
+import { Link } from "react-router-dom";
 
 export default function NavBar() {
     return (
-        <div className="navbar-container">
-            <div className="logo-name-container">
-                <img className="logo" src="#" alt="image-logo"/>
-                <p className="logo-text">Fantasy Soccer</p>
-            </div>
-            <button className="navbar-btn" type="button">Create Fantasy Team</button>
-        </div>
+        <nav className="nav">
+            
+            <Link to="/home">
+                <p className="logo">
+                    <img src="src/assets/fs-logo-without-background.png" alt="logo" className="w-8 h-8 rounded-full" />
+                </p>
+            </Link>
+
+            <Link to="/">
+                <p>Login</p>
+            </Link>
+
+            <Link to="/home">
+                <p>Home</p>
+            </Link>
+
+            <Link to="/stats">
+                <p>Player Directory</p>
+            </Link>
+
+            <Link to="/stats/add">
+                <p>Add</p>
+            </Link>
+
+            <Link to="/about">
+                <p>About</p>
+            </Link>
+
+        </nav>
     )
 }
+
+
+
+/* basic red: #FE3037
+dark red: #AE1D1E
+black: #1E1414
+white: #FFF */
